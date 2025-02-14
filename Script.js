6d254b6a28ced038f4a427ac1677ee62b74a7ee2
Guilherme <147566218+Guilherm001@users.noreply.github.com>
2025-02-14 14:31:14 +0000

    adicionando imgs ao projeto

--- Script.js
+++ Script.js
@@ -6,12 +6,15 @@ const banner = document.querySelector('app__image')
 
 focoBt.addEventListener( 'click', () =>{
     html.setAttribute('data-contexto', 'foco')
+    banner.setAttribute('src', '/imagens/foco.png')
 
 });
 curtoBt.addEventListener('click', () => {
     html.setAttribute('data-contexto', 'descanso-curto')
+    banner.setAttribute('src', '/imagens/descanso-curto.png')
 })
 
 longoBt.addEventListener('click', () =>{
     html.setAttribute('data-contexto', 'descanso-longo')
+    banner.setAttribute('src', 'descanso-longo.png')
 })
